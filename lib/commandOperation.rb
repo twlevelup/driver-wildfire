5fc7c9d85c8f2fc290984a02537a8ace31166242
fnliu <liufeng501@hotmail.com>
2014-09-09 19:07:07 +1000

    Check commands is F,L or R and make strings case insensitive

--- lib/commandOperation.rb
+++ lib/commandOperation.rb
@@ -10,12 +10,17 @@ class CommandOperation
        if positionDirection.size() != 3
           raise ArgumentError, 'Please input initial location following this format (Location_X, Location_Y, Direction)'
        end
-       @car = Car.new(Position.new(positionDirection[0].to_i, positionDirection[1].to_i), positionDirection[2].to_sym)
-       @moveCommand = moveCommand
+       @car = Car.new(Position.new(positionDirection[0].to_i, positionDirection[1].to_i), positionDirection[2].upcase.to_sym)
+       @moveCommand = moveCommand.upcase
     end
   end
   
   def getLocation()
+     @moveCommand.each_char.each do |c|
+        if(!c.eql?("L") && !c.eql?("F") && !c.eql?("R"))
+           raise ArgumentError, 'Commands must be L, F or R'
+        end
+     end
      car.stackCommands(@moveCommand.each_char.to_a.map &:to_sym)
      return "New location: (#{@car.position.x} , #{@car.position.y}), facing to #{@car.direction.to_s}" 
   end
