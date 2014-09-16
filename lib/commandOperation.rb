f02248dec2c77e8ffb5a268bc57e8e544b664d42
changboyu <chang.boyu88@gmail.com>
2014-09-16 15:31:58 +1000

    refatory of dealing with the input arguments

--- lib/commandOperation.rb
+++ lib/commandOperation.rb
@@ -5,27 +5,17 @@ require './lib/grid'
 
 class CommandOperation
   attr_accessor :car
-  attr_accessor :move_command 
+  attr_accessor :move_commands
   attr_accessor :car_index
-  def initialize(initialLocation, move_command)
-    begin
-       @car_index = 0
-       position_direction = initialLocation.split(',')
-       if position_direction.size() != 3
-          raise ArgumentError, 'Please input initial location following this format (Location_X, Location_Y, Direction)'
-       end
-       @car = Car.new(@car_index, Grid.new(0,50,0,50), Position.new(position_direction[0].to_i, position_direction[1].to_i), position_direction[2].upcase.to_sym)
-       @move_command = move_command.upcase
-    end
+  def initialize(initial_arguments, move_commands)
+    @car_index = 0
+    @car = Car.new(@car_index, Grid.new(0,50,0,50), Position.new(initial_arguments[0].to_i, initial_arguments[1].to_i), initial_arguments[2].upcase.to_sym)
+    @move_commands = move_commands.upcase
   end
   
-  def getLocation()
-     @move_command.each_char.each do |c|
-        if(!c.eql?("L") && !c.eql?("F") && !c.eql?("R"))
-           raise ArgumentError, 'Commands must be L, F or R'
-        end
-     end
-     car.stack_commands(@move_command.each_char.to_a.map &:to_sym)
+  def get_location()
+     
+     car.stack_commands(@move_commands.each_char.to_a.map &:to_sym)
      return "New location: (#{@car.position.x} , #{@car.position.y}), facing to #{@car.direction.to_s}" 
   end
 end
--- operator.rb
+++ operator.rb
@@ -7,8 +7,34 @@ begin
     raise ArgumentError, 'Please input 2 arguments!'
   end
   
-  commandOperation = CommandOperation.new(ARGV[0], ARGV[1]) 
-  puts commandOperation.getLocation()
+  origin_initial_arguments = ARGV[0].split(',')
+  origin_move_commands = ARGV[1]
+  car_initial_arguments = nil
+  car_move_commands = nil
+
+  if origin_initial_arguments.size() != 3
+    raise ArgumentError, 'Please input initial location following this format (Location_X, Location_Y, Direction)'
+  end
+
+  # match the input arguments with a nondigit: /[^0-9]/
+  if ((origin_initial_arguments[0] =~/\D/) || (origin_initial_arguments[1] =~/\D/))
+    raise ArgumentError, 'The position arguments must be integer'
+  else car_initial_arguments = origin_initial_arguments
+  end
+
+  # limit the move commands to F,R,L
+  if origin_move_commands =~ /[^FfLlRr]/
+    raise ArgumentError, 'Move commands must be L, F or R'
+  else car_move_commands = origin_move_commands
+  end
+
+  #grid_initial = ARGV[3]
+  #ToDo
+
+  #destination = ARGV[4]
+
+  command_operation = CommandOperation.new(car_initial_arguments, car_move_commands)
+  puts command_operation.get_location()
   rescue ArgumentError => e
-    puts e.message 
+    puts e.message
 end           
