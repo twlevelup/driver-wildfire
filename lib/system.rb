750eff10eeff9cfc4dada06f019827a3f4dc973d
changboyu <chang.boyu88@gmail.com>
2014-09-16 17:44:04 +1000

    add system file to achieve run multible taxies

---
+++ lib/system.rb
@@ -0,0 +1,52 @@
+require './car'
+require './grid'
+require './position'
+
+begin
+
+  grid = nil
+  taxi_list = Array.new
+  taxi_index = 0
+  taxi = nil
+  
+  while true
+    puts "Please input the commands..."
+    msg = gets.chomp
+    case msg 
+      when "exit"
+        raise ArgumentError, 'The system has been shut down.'
+      when "help"
+        puts "Basic Commands: createtaxi"
+        puts "                destination"
+        puts "                creatgrid"
+        puts "                taxi"
+        puts "                givecommand"
+      when "createtaxi"
+        puts "Please define the arguments for taxi (x,y,direction)..."
+        initial_arguments = gets.chomp.split(',')
+        taxi = Car.new(taxi_index,grid,Position.new(initial_arguments[0].to_i,initial_arguments[1].to_i),initial_arguments[2].upcase.to_sym)
+        puts "Taxi #{taxi_index} has been created successfully."
+        puts "Taxi #{taxi.index} : #{taxi.position.x} , #{taxi.position.y}  "
+        taxi_list << taxi
+        taxi_index += 1
+      when "creategrid"
+        puts "Please input the arguments for grid"
+        grid_arguments = gets.chomp().split(',')
+        grid = Grid.new(grid_arguments[0].to_i,grid_arguments[1].to_i,grid_arguments[2].to_i,grid_arguments[3].to_i)
+        grid.print_grid
+        puts "Grid has been successfully created #{}"
+      when "taxishow"
+        taxi_list.each do |item|
+          puts "taxi: #{item.index}"
+        end
+      when "showgrid"
+        grid.print_grid
+    end
+
+
+  end
+
+rescue ArgumentError => e
+  puts e.message
+
+end
